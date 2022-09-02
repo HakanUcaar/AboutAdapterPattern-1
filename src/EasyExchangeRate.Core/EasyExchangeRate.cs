c90a53b04a775530214eeea8fceba4826b343fa1
Hakan Uçar <hakanucaar@yandex.com>
2022-09-03 00:30:04 +0300

    AboutAdapterPattern

---
+++ src/EasyExchangeRate.Core/EasyExchangeRate.cs
@@ -0,0 +1,27 @@
+﻿using EasyExchangeRate.Core.Abstraction;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EasyExchangeRate.Core
+{
+    public class EasyExchangeRate
+    {
+        private static readonly HashSet<IExchangeRateAdapter> Adapters = new HashSet<IExchangeRateAdapter>();
+        public EasyExchangeRate()
+        {
+
+        }
+
+        public HashSet<IExchangeRateAdapter> GetAdapters()
+        {
+            return Adapters;
+        }
+        public void AddAdapter(IExchangeRateAdapter adapter)
+        {
+            Adapters.Add(adapter);
+        }
+    }
+}
---
+++ src/EasyExchangeRate.Test/Program.cs
@@ -0,0 +1,43 @@
+﻿
+using EasyExchangeRate.Core.Common;
+using EasyExchangeRate.Denmark;
+using EasyExchangeRate.Europe;
+using EasyExchangeRate.Israel;
+using EasyExchangeRate.Turkey;
+using System;
+
+namespace EasyExchangeRate.Test
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //GetAdapterCurrencies();
+            GetAdapterRates();
+
+            Console.ReadLine();
+        }
+
+        static void GetAdapterCurrencies()
+        {
+            TurkeyAdapter adapter = new TurkeyAdapter();
+            adapter.Currencies.ForEach(c =>
+            {
+                Console.WriteLine(c.ToString());
+            });
+        }
+
+        static void GetAdapterRates()
+        {
+            TurkeyAdapter adapter = new TurkeyAdapter();
+            Console.WriteLine($"Base Currency : {adapter.BaseCurrency.Value.Name}");
+            Console.WriteLine("Rates :");
+            adapter.Rates.ForEach(c =>
+            {
+                Console.WriteLine($"{c.Value.Currency.Value.Name} = " + c.Value.Amount);
+            });
+        }
+
+        
+    }
+}
